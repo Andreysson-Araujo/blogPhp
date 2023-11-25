e3775ef86996d5a60ff0e4ea691089b95efaf641
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-11-24 23:14:18 -0500

    realizando criação e divisão dos templates

--- index.php
+++ index.php
@@ -1,22 +1,7 @@
 <?php
-include_once("helpers/url.php")
-    ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BlogCodar</title>
-    <!--ESTILOS DO PROJETO -->
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
-    <!-- GOOGLE FONTS -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
-</head>
-<body>
-    <h1>Test de Elemento</h1>
-</body>
-</html>
+    include_once("templates/header.php")
+?>
+    <h1>Um possivel titulo</h1>
+    <?php
+    include_once("templates/footer.php")    
+?>
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>HoraCodar &copy; 2023</p>
+</footer>
+</body>
+</html>
