3648f397b9e6f72fad9a05c9ece26e7e1f7837f9
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-11-24 22:46:42 -0500

    commit inicial

---
+++ data/categories.php
@@ -0,0 +1,13 @@
+<?php
+    $categories = [
+        "PHP",
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "JavaScript",
+        "Bootstap",
+        "Java",
+        "C#"
+    ];
+    print_r($categories[0]);
