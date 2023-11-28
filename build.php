c0affd4ce7d94ec1aab255a0e419c7b98a0bc8be
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2023-11-28 17:31:19 -0500

    tentativa 9

--- build.php
+++ build.php
@@ -16,7 +16,7 @@ ob_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Drak Blog</title>
-    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
